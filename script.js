5cd70fe45b8752ecf0e06889a769d873bfdc9193
Pratikshapulate-1845 <pratikshapulate@gmail.com>
2026-04-04 19:12:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const apiKey = "60c470ccc9a6215502407fa9f0d74a37";
+
+async function getWeather() {
+  const city = document.getElementById("city").value;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data); // 👈 IMPORTANT
+
+    if (data.cod !== 200) {
+      document.getElementById("weather").innerHTML = data.message;
+      return;
+    }
+
+    document.getElementById("weather").innerHTML = `
+      <h2>${data.name}</h2>
+      <p>Temperature: ${data.main.temp} °C</p>
+      <p>Weather: ${data.weather[0].description}</p>
+    `;
+  } catch (error) {
+    console.error(error);
+    document.getElementById("weather").innerHTML = "Error fetching data";
+  }
+}
